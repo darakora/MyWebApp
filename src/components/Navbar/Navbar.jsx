20780839be96ceaf91933eee38c865f4254e0dda
darakora <43516552+darakora@users.noreply.github.com>
2022-02-24 01:56:00 +0300

    add footer (#10)

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -44,28 +44,25 @@ const Navbar = () => {
           fontSize={27}
           onClick={() => setToggleMenu(true)}
         ></BiMenu>
-        {toggleMenu && (
+       
+       {toggleMenu && (
           <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
-            <MdOutlineRestaurantMenu
-              fontSize={27}
-              className="overlay__close"
-              onClick={() => setToggleMenu(false)}
-            />
+            <MdOutlineRestaurantMenu fontSize={27} className="overlay__close" onClick={() => setToggleMenu(false)} />
             <ul className="app__navbar-smallscreen_links">
               <li className="p__opensans">
-                <a href="#home">Home</a>
+                <a href="#home" onClick={() => setToggleMenu(false)}>Home</a>
               </li>
               <li className="p__opensans">
-                <a href="#about">About</a>
+                <a href="#about" onClick={() => setToggleMenu(false)}>About</a>
               </li>
               <li className="p__opensans">
-                <a href="#menu">Menu</a>
+                <a href="#menu" onClick={() => setToggleMenu(false)}>Menu</a>
               </li>
               <li className="p__opensans">
-                <a href="#awards">Awards</a>
+                <a href="#awards" onClick={() => setToggleMenu(false)}>Awards</a>
               </li>
               <li className="p__opensans">
-                <a href="#contact">Contact</a>
+                <a href="#contact" onClick={() => setToggleMenu(false)}>Contact</a>
               </li>
             </ul>
           </div>
--- src/components/index.js
+++ src/components/index.js
@@ -9,5 +9,5 @@ export {
   Newsletter,
   FooterOverlay,
   MenuItem,
-  Navbar,
+  Navbar
 };
